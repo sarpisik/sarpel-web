2cc2756bf337b397dda6a196a8ae3ead74038b24
Sarp ISIK <wdsarpisik@gmail.com>
2021-02-13 13:10:31 +0300

    Add alias-imports

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Sarpel Elektrik',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.'
+      'Sarpel Elektrik "Doğadan Aldığımız Güçle Her Yerdeyiz" sloganıyla yola çıkarak, vizyonu elektrik mühendisliği hizmetlerinin inovatif ve lider kuruluşu olmasıdır'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -10,6 +10,21 @@ module.exports = {
       resolve: 'gatsby-plugin-sass',
       options: {
         indentedSyntax: true
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-alias-imports',
+      options: {
+        alias: {
+          '@plugins': 'plugins',
+          '@src': 'src',
+          '@assets': 'src/assets',
+          '@components': 'src/components',
+          '@pages': 'src/pages',
+          '@templates': 'src/templates',
+          '@views': 'src/views'
+        },
+        extensions: ['js']
       }
     },
     {
