7a7268f1ca652da4572caa0192028a4b615c27a8
Sarp ISIK <wdsarpisik@gmail.com>
2021-04-07 16:20:25 +0300

    Edit style "Caption"

--- src/components/caption/Caption.js
+++ src/components/caption/Caption.js
@@ -9,14 +9,19 @@ const StyledCaption = withStyles(theme => ({
     top: 0,
     left: 0,
     backgroundColor: 'rgba(0,0,0,0.5)',
-    padding: theme.spacing(0.5, 1)
+    padding: theme.spacing(0.5, 1),
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 }))(Box)
 
 export function Caption(props) {
   return (
     <StyledCaption>
-      <Typography {...props} variant="caption" color="initial" />
+      <Typography {...props} variant="h3" component="span" color="initial" />
     </StyledCaption>
   )
 }
