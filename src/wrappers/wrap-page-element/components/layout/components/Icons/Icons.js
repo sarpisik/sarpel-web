e3a14b3c6a5654f40ce3bd5ae233a6fa38dd2889
Sarp ISIK <wdsarpisik@gmail.com>
2021-04-12 21:40:13 +0300

    Edit footer icon size

--- src/wrappers/wrap-page-element/components/layout/components/Icons/Icons.js
+++ src/wrappers/wrap-page-element/components/layout/components/Icons/Icons.js
@@ -2,13 +2,15 @@ import { Box, Link } from '@material-ui/core'
 import MailIcon from '@material-ui/icons/Mail'
 import React from 'react'
 
+const ICON_STYLE = { fontSize: 40 }
+
 export default function Icons(props) {
   const { email } = props
 
   return (
     <Box display="flex" justifyContent="center" className="footer-child">
       <Link href={`mailto:${email}`}>
-        <MailIcon />
+        <MailIcon style={ICON_STYLE} />
       </Link>
     </Box>
   )
