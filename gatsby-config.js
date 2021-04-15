3851b9f1b81a7dbda2a3cf26729ea7c8fe3e1749
Sarp ISIK <wdsarpisik@gmail.com>
2021-04-15 20:09:28 +0300

    Add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,6 +107,10 @@ module.exports = {
       }
     },
     {
+      resolve: 'gatsby-plugin-sitemap',
+      options: { exclude: ['/admin'] }
+    },
+    {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`
