f0f6f1f3d6a6e27312fdccfcbc4699f61e711a0f
Sarp ISIK <wdsarpisik@gmail.com>
2021-04-15 07:48:43 +0300

    Fix bug export of IndexPageTemplate

---
+++ src/components/IndexPageTemplate.js
@@ -0,0 +1,109 @@
+import { Banner } from '@components/banner'
+import CardWithHeight from '@components/CardWithHeight'
+import CardWithMedia from '@components/CardWithMedia'
+import { Container } from '@components/container'
+import {
+  Section,
+  SectionCardContainer,
+  SectionSubTitle
+} from '@components/section'
+import Box from '@material-ui/core/Box'
+import CardContent from '@material-ui/core/CardContent'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import EcoIcon from '@material-ui/icons/Eco'
+import GroupIcon from '@material-ui/icons/Group'
+import HowToRegIcon from '@material-ui/icons/HowToReg'
+import StarIcon from '@material-ui/icons/Star'
+import TurnedInIcon from '@material-ui/icons/TurnedIn'
+import PropTypes from 'prop-types'
+import React from 'react'
+import Carousel from 'react-material-ui-carousel'
+
+const ICONS = {
+  star: StarIcon,
+  group: GroupIcon,
+  howToReg: HowToRegIcon,
+  turnedIn: TurnedInIcon,
+  eco: EcoIcon
+}
+
+export const IndexPageTemplate = props => {
+  const { banners, whatWeDo, whyUs } = props
+
+  return (
+    <React.Fragment>
+      {/* Banners */}
+      <Carousel>{banners.map(renderBanner)}</Carousel>
+
+      <Container padding={5}>
+        <Grid container spacing={5}>
+          {/* What We Do */}
+          <Section title={whatWeDo.title}>
+            {whatWeDo.works.map(work => {
+              const { title, description, ...image } = work
+
+              return (
+                <SectionCardContainer key={title}>
+                  <CardWithMedia {...image}>
+                    <SectionSubTitle>{title}</SectionSubTitle>
+                    <Typography variant="body1" color="initial" align="justify">
+                      {description}
+                    </Typography>
+                  </CardWithMedia>
+                </SectionCardContainer>
+              )
+            })}
+          </Section>
+
+          {/* Why Us */}
+          <Section title={whyUs.title}>
+            {whyUs.reasons.map(reason => {
+              const { title, description, icon } = reason,
+                Icon = ICONS[icon]
+
+              return (
+                <SectionCardContainer key={title}>
+                  <CardWithHeight>
+                    {Icon && (
+                      <CardContent>
+                        <Box textAlign="center">
+                          <Icon style={{ fontSize: 80 }} />
+                        </Box>
+                      </CardContent>
+                    )}
+                    <CardContent>
+                      <SectionSubTitle align="center">{title}</SectionSubTitle>
+                      <Typography
+                        variant="body1"
+                        color="initial"
+                        align="center"
+                      >
+                        {description}
+                      </Typography>
+                    </CardContent>
+                  </CardWithHeight>
+                </SectionCardContainer>
+              )
+            })}
+          </Section>
+        </Grid>
+      </Container>
+    </React.Fragment>
+  )
+}
+IndexPageTemplate.propTypes = {
+  whatWeDo: PropTypes.shape({ title: PropTypes.string.isRequired }),
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  title: PropTypes.string,
+  heading: PropTypes.string,
+  subheading: PropTypes.string,
+  mainpitch: PropTypes.object,
+  description: PropTypes.string,
+  intro: PropTypes.shape({
+    blurbs: PropTypes.array
+  })
+}
+function renderBanner(props) {
+  return <Banner key={props.text} {...props} />
+}
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,113 +1,8 @@
-import CardWithMedia from '@components/CardWithMedia'
-import Box from '@material-ui/core/Box'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
 import { graphql } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import Carousel from 'react-material-ui-carousel'
-
-import StarIcon from '@material-ui/icons/Star'
-import GroupIcon from '@material-ui/icons/Group'
-import HowToRegIcon from '@material-ui/icons/HowToReg'
-import TurnedInIcon from '@material-ui/icons/TurnedIn'
-import EcoIcon from '@material-ui/icons/Eco'
-import CardWithHeight from '@components/CardWithHeight'
-import CardContent from '@material-ui/core/CardContent'
-import { Banner } from '@components/banner'
-import { Container } from '@components/container'
-import {
-  Section,
-  SectionSubTitle,
-  SectionCardContainer
-} from '@components/section'
-
-const ICONS = {
-  star: StarIcon,
-  group: GroupIcon,
-  howToReg: HowToRegIcon,
-  turnedIn: TurnedInIcon,
-  eco: EcoIcon
-}
-
-export const IndexPageTemplate = props => {
-  const { banners, whatWeDo, whyUs } = props
-
-  return (
-    <React.Fragment>
-      {/* Banners */}
-      <Carousel>{banners.map(renderBanner)}</Carousel>
-
-      <Container padding={5}>
-        <Grid container spacing={5}>
-          {/* What We Do */}
-          <Section title={whatWeDo.title}>
-            {whatWeDo.works.map(work => {
-              const { title, description, ...image } = work
-
-              return (
-                <SectionCardContainer key={title}>
-                  <CardWithMedia {...image}>
-                    <SectionSubTitle>{title}</SectionSubTitle>
-                    <Typography variant="body1" color="initial" align="justify">
-                      {description}
-                    </Typography>
-                  </CardWithMedia>
-                </SectionCardContainer>
-              )
-            })}
-          </Section>
-
-          {/* Why Us */}
-          <Section title={whyUs.title}>
-            {whyUs.reasons.map(reason => {
-              const { title, description, icon } = reason,
-                Icon = ICONS[icon]
-
-              return (
-                <SectionCardContainer key={title}>
-                  <CardWithHeight>
-                    {Icon && (
-                      <CardContent>
-                        <Box textAlign="center">
-                          <Icon style={{ fontSize: 80 }} />
-                        </Box>
-                      </CardContent>
-                    )}
-                    <CardContent>
-                      <SectionSubTitle align="center">{title}</SectionSubTitle>
-                      <Typography
-                        variant="body1"
-                        color="initial"
-                        align="center"
-                      >
-                        {description}
-                      </Typography>
-                    </CardContent>
-                  </CardWithHeight>
-                </SectionCardContainer>
-              )
-            })}
-          </Section>
-        </Grid>
-      </Container>
-    </React.Fragment>
-  )
-}
-
-IndexPageTemplate.propTypes = {
-  whatWeDo: PropTypes.shape({ title: PropTypes.string.isRequired }),
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array
-  })
-}
+import { IndexPageTemplate } from '@components/IndexPageTemplate'
 
 const IndexPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark
@@ -170,7 +65,3 @@ export const pageQuery = graphql`
     }
   }
 `
-
-function renderBanner(props) {
-  return <Banner key={props.text} {...props} />
-}
