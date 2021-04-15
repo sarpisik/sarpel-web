ac9fc9b528c7f75c7742a0a771626664acaec532
Sarp ISIK <wdsarpisik@gmail.com>
2021-04-15 14:09:34 +0300

    Edit component SEO

--- src/components/seo/Seo.js
+++ src/components/seo/Seo.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
+import { useSiteMetadata } from './hooks'
 
 export function SEO(props) {
-  const { title, description } = props
+  const { title, description } = props,
+    siteMetadata = useSiteMetadata()
 
-  return <Helmet title={title} description={description} />
+  return (
+    <Helmet
+      title={title}
+      description={description}
+      titleTemplate={`${siteMetadata.title} | %s`}
+    />
+  )
 }
 
-// SEO.defaultProps={
-//   title:'',description:''
-// }
+SEO.defaultProps = {
+  description: ''
+}
 
 SEO.propTypes = {
   title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired
+  description: PropTypes.string
 }
