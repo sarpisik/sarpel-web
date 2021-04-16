1ab4a4ff555d66a765576ed70193e691c9507b8e
Sarp ISIK <wdsarpisik@gmail.com>
2021-04-16 22:57:34 +0300

    Add preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     siteUrl: process.env.SITE_URL || 'http://localhost:8000'
   },
   plugins: [
+    `gatsby-plugin-preact`,
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-alias-imports',
