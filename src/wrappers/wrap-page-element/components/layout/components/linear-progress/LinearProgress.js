e5f339fac8f457866f6d8f506f116c2e496492b5
Sarp ISIK <wdsarpisik@gmail.com>
2021-04-17 21:42:22 +0300

    Edit import mui

--- src/wrappers/wrap-page-element/components/layout/components/linear-progress/LinearProgress.js
+++ src/wrappers/wrap-page-element/components/layout/components/linear-progress/LinearProgress.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import MuiLinearProgress from '@material-ui/core/LinearProgress'
+import { LinearProgress as MuiLinearProgress } from '@material-ui/core'
 
 export function LinearProgress() {
   return <MuiLinearProgress color="primary" />
