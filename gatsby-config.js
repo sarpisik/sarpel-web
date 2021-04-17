7b35ab613a7861f0ce1693093baf7148dfaacd27
Sarp ISIK <wdsarpisik@gmail.com>
2021-04-17 19:21:12 +0300

    Fix plugin "netlify"

--- gatsby-config.js
+++ gatsby-config.js
@@ -133,7 +133,6 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`
       }
     },
-    'gatsby-plugin-webpack-bundle-analyser-v2',
-    'gatsby-plugin-netlify' // make sure to keep it last in the array
+    'gatsby-plugin-webpack-bundle-analyser-v2'
   ]
 }
