27fe2a9b55b87f5bc5d7a2e708044348a0372dea
Sarp ISIK <wdsarpisik@gmail.com>
2021-04-18 19:59:15 +0300

    Fix mui plugin

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,10 +13,6 @@ exports.onCreateBabelConfig = ({ actions }) => {
         transform: '@material-ui/core/esm/${member}',
         preventFullImport: true
       },
-      '@material-ui/core/styles': {
-        transform: '@material-ui/core/esm/styles/${member}',
-        preventFullImport: true
-      },
       '@material-ui/icons': {
         // Use "transform: '@material-ui/icons/${member}'," if your bundler does not support ES modules
         transform: '@material-ui/icons/esm/${member}',
--- plugins/gatsby-plugin-mui-layout/ThemeProvider.js
+++ plugins/gatsby-plugin-mui-layout/ThemeProvider.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { CssBaseline } from '@material-ui/core'
-import { MuiThemeProvider as TP } from '@material-ui/core/styles'
+import { ThemeProvider as TP } from '@material-ui/core/styles'
 
 import { useDarkMode } from '../gatsby-plugin-dark-mode/DarkModeProvider'
 import { getThemeByName } from './themes'
